682c5c1c64307964eacc7f695f532f595b99c44c
Matthew Petroff <matthew@mpetroff.net>
2012-04-19 16:26:05 -0400

    Made code style mode consistent.

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -17,23 +17,21 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-if(getURLParameter('logo') == 'yes')
-{
+if(getURLParameter('logo') == 'yes') {
 	document.getElementById('pannellum_logo').style.display = 'inline';
 }
 
-if(getURLParameter('title'))
-{
+if(getURLParameter('title')) {
 	document.getElementById('title_box').innerHTML = getURLParameter('title');
 }
 
-if(getURLParameter('author'))
-{
+if(getURLParameter('author')) {
 	document.getElementById('author_box').innerHTML = 'by ' + getURLParameter('author');
 }
+
 var popoutmode = false;
-if(getURLParameter('popout') == 'yes')
-{
+
+if(getURLParameter('popout') == 'yes') {
 	document.getElementById('fullwindowtoggle_button').id = 'fullwindowtoggle_button_active';
 	popoutmode = true;
 }
@@ -54,26 +52,21 @@ var isTimedOut = false;
 
 var about_box = document.getElementById('about_box');
 
-if(getURLParameter('autoload') == 'yes' || getURLParameter('popoutautoload') == 'yes')
-{
-	if(getURLParameter('popoutautoload') != 'yes')
-	{
+if(getURLParameter('autoload') == 'yes' || getURLParameter('popoutautoload') == 'yes') {
+	if(getURLParameter('popoutautoload') != 'yes') {
 		// show loading box
 		document.getElementById('load_box').load_box.style.display = 'inline';
 	}
 	// initialize
 	init();
 	animate();
-}
-else
-{
+} else {
 	// show load button
 	var load_button = document.getElementById('load_button');
 	load_button.style.display = 'table';
 }
 
-function init()
-{
+function init() {
 	var container, mesh;
 	
 	container = document.getElementById('container');
@@ -83,33 +76,26 @@ function init()
 	scene = new THREE.Scene();
 	
 	var panoimage = new Image(),panotexture = new THREE.Texture(panoimage);
-	panoimage.onload = function()
-	{
+	panoimage.onload = function() {
 		//var panotexture = new THREE.Texture(panoimage);
 		
 		panotexture.needsUpdate = true;
 		mesh = new THREE.Mesh(new THREE.Sphere(500,60,40), new THREE.MeshBasicMaterial({map:panotexture}));
 		mesh.scale.x = -1;
-		try
-		{
+		try {
 			scene.addObject(mesh);
-		}
-		catch (event)
-		{
+		} catch (event) {
 			// show error message if canvas is not supported
 			load_box.style.display = 'none';
 			document.getElementById('nocanvas').style.display = 'table';
 		}
 		
 		// try to use WebGL, else fallback to 2D canvas
-		try
-		{
+		try {
 			renderer = new THREE.WebGLRenderer();
 			renderer.setSize(window.innerWidth,window.innerHeight);
 			renderer.initWebGLObjects(scene);
-		}
-		catch (event)
-		{
+		} catch (event) {
 			// show error message if WebGl is not supported
 			load_box.style.display = 'none';
 			document.getElementById('nocanvas').style.display = 'table';
@@ -126,16 +112,15 @@ function init()
 		
 		document.onkeydown = onDocumentKeyPress;
 		
-		renderinit();
-		var t=setTimeout("isTimedOut = true",500);
+		renderInit();
+		var t=setTimeout('isTimedOut = true',500);
 	};
 	panoimage.src = getURLParameter('panorama');
 	
 	document.getElementById('page').className = 'grab';
 }
 
-function onDocumentMouseDown(event)
-{
+function onDocumentMouseDown(event) {
 	event.preventDefault();
 	
 	isUserInteracting = true;
@@ -149,148 +134,91 @@ function onDocumentMouseDown(event)
 	document.getElementById('page').className = 'grabbing';
 }
 
-function onDocumentMouseMove(event)
-{
-	if (isUserInteracting)
-	{		
+function onDocumentMouseMove(event) {
+	if (isUserInteracting) {		
 		lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
 		lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
 		animate();	
 	}
 }
 
-function onDocumentMouseUp(event)
-{
+function onDocumentMouseUp(event) {
 	isUserInteracting = false;
 	document.getElementById('page').className = 'grab';
 }
 
-function onDocumentMouseWheel(event)
-{
-	if (fov >= 35 && fov <= 105)
-	{	
-		// WebKit
-		if (event.wheelDeltaY)
-		{
+function onDocumentMouseWheel(event) {
+	if (fov >= 35 && fov <= 105) {
+		if (event.wheelDeltaY) {
+			// WebKit
 			fov -= event.wheelDeltaY * 0.05;
-		}
-		// Opera / Explorer 9
-		else if (event.wheelDelta)
-		{
+		} else if (event.wheelDelta) {
+			// Opera / Explorer 9
 			fov -= event.wheelDelta * 0.05;
-		}
-		// Firefox
-		else if (event.detail)
-		{
+		} else if (event.detail) {
+			// Firefox
 			fov += event.detail * 1.5;
 		}
 	}
 	
 	// keep field of view within bounds
-	if(fov < 35)
-	{
+	if(fov < 35) {
 		fov = 35;
-	}
-	if(fov > 105)
-	{
+	} else if(fov > 105) {
 		fov = 105;
 	}
 	camera.projectionMatrix = THREE.Matrix4.makePerspective(fov,window.innerWidth / window.innerHeight,1,1100);
 	render();
 }
 
-function onDocumentKeyPress (event)
-{
+function onDocumentKeyPress (event) {
 	// record key pressed
 	keynumber = event.keycode;
-	if(event.which)
-	{
+	if(event.which) {
 		keynumber = event.which;
 	}
 	
 	// if minus key is pressed
-	if(keynumber == 109)
-	{
-		// zoom out
-		if(fov <= 100)
-		{
-			fov += 5;
-			camera.projectionMatrix = THREE.Matrix4.makePerspective(fov,window.innerWidth / window.innerHeight,1,1100);
-			render();
-		}
-		// keep field of view within bounds
-		if(fov < 40)
-		{
-			fov = 40;
-		}
-		if(fov > 100)
-		{
-			fov = 100;
-		}
+	if(keynumber == 109) {
+		zoomOut();
 	}
 	
 	// if plus key is pressed
-	if(keynumber == 107)
-	{
-		// zoom in
-		if(fov >= 40)
-		{
-			fov -= 5;
-			camera.projectionMatrix = THREE.Matrix4.makePerspective(fov,window.innerWidth / window.innerHeight,1,1100);
-			render();
-		}
-		// keep field of view within bounds
-		if(fov < 40)
-		{
-			fov = 40;
-		}
-		if(fov > 100)
-		{
-			fov = 100;
-		}
+	if(keynumber == 107) {
+		zoomIn();
 	}
 	
 	// if in full window / popout mode
-	if(fullWindowActive == true || popoutmode == true)
-	{
+	if(fullWindowActive == true || popoutmode == true) {
 		// if escape key is pressed
-		if(keynumber == 27)
-		{
+		if(keynumber == 27) {
 			toggleFullWindow();
 		}
 	}
 }
 
 window.onresize = function(){onDocumentResize();};
-function onDocumentResize()
-{
+function onDocumentResize() {
 	// reset panorama renderer
-	try
-	{
+	try {
 		camera.aspect = window.innerWidth / window.innerHeight;
 		renderer.setSize(window.innerWidth,window.innerHeight);
 		camera.projectionMatrix = THREE.Matrix4.makePerspective(fov,window.innerWidth / window.innerHeight,1,1100);
 		render();
-	}
-	catch(event)
-	{
+	} catch(event) {
 		// panorama not loaded
 	}
 }
 
-function animate()
-{
+function animate() {
 	render();
-	if(isUserInteracting)
-	{
+	if(isUserInteracting) {
 		requestAnimationFrame(animate);
 	}
 }
 
-function render()
-{
-	try
-	{
+function render() {
+	try {
 		lat = Math.max(-85,Math.min(85,lat));
 		phi = (90 - lat) * Math.PI / 180;
 		theta = lon * Math.PI / 180;
@@ -300,61 +228,45 @@ function render()
 		camera.target.position.z = 500 * Math.sin(phi) * Math.sin(theta);
 		
 		renderer.render(scene,camera);
-	}
-	catch(event)
-	{
+	} catch(event) {
 		// panorama not loaded
 	}
 }
 
-function renderinit()
-{
-	//try
-	//{
+function renderInit() {
+	try {
 		camera.target.x = 0;
 		camera.target.y = 0;
 		camera.target.z = 0;
 		
 		renderer.render(scene,camera);
 		
-		if(!isTimedOut)
-		{
-			requestAnimationFrame(renderinit);
-		}
-		else
-		{
+		if(!isTimedOut) {
+			requestAnimationFrame(renderInit);
+		} else {
 			// hide loading display
 			document.getElementById('load_box').style.display = 'none';
 		}
-	//}
-	//catch(event)
-	//{
+	} catch(event) {
 		// panorama not loaded
-	//}
+	}
 }
 
-function getURLParameter(name)
-{
-	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-	var regexS = "[\\?&]"+name+"=([^&#]*)";
+function getURLParameter(name) {
+	name = name.replace(/[\[]/,'\\\[').replace(/[\]]/,'\\\]');
+	var regexS = '[\\?&]'+name+'=([^&#]*)';
 	var regex = new RegExp(regexS);
 	var results = regex.exec(unescape(window.location.href))
-	if(results == null)
-	{
-		return "";
-	}
-	else
-	{
+	if(results == null) {
+		return '';
+	} else {
 		return results[1];
 	}
 }
 
-function toggleFullWindow()
-{
-	if(fullWindowActive == false)
-	{
-		try
-		{
+function toggleFullWindow() {
+	if(fullWindowActive == false) {
+		try {
 			var page = document.getElementById('page');
 			if (page.requestFullscreen) {
 				page.requestFullscreen();
@@ -366,40 +278,28 @@ function toggleFullWindow()
 			
 			document.getElementById('fullwindowtoggle_button').id = 'fullwindowtoggle_button_active';
 			fullWindowActive = true;
-		}
-		catch(event)
-		{
-			if(getURLParameter('popout') != 'yes')
-			{
+		} catch(event) {
+			if(getURLParameter('popout') != 'yes') {
 				// open new window instead
 				var windowspecs = 'width=' + screen.width + ',height=' + screen.height + ',left=0,top=0';
 				var windowlocation = window.location.href + '&popout=yes';
-				try
-				{
+				try {
 					camera.aspect = window.innerWidth / window.innerHeight;
 					windowlocation += '&popoutautoload=yes';
-				}
-				catch(event)
-				{
+				} catch(event) {
 					// panorama not loaded
 				}
 				window.open(windowlocation,null,windowspecs)
-			}
-			else
-			{
+			} else {
 				window.close();
 			}
 		}
-	}
-	else
-	{
+	} else {
 		if (document.exitFullscreen) {
 			document.exitFullscreen();
-		}
-		else if (document.mozCancelFullScreen) {
+		} else if (document.mozCancelFullScreen) {
 			document.mozCancelFullScreen();
-		}
-		else if (document.webkitCancelFullScreen) {
+		} else if (document.webkitCancelFullScreen) {
 			document.webkitCancelFullScreen();
 		}
 		
@@ -415,16 +315,28 @@ function toggleFullWindow()
 function zoomIn() {
 	if( fov >= 40 ) {
 		fov -= 5;
-		camera.projectionMatrix = THREE.Matrix4.makePerspective( fov, window.innerWidth / window.innerHeight, 1, 1100 );
+		camera.projectionMatrix = THREE.Matrix4.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
 		render();
+	}
+	// keep field of view within bounds
+	if(fov < 40) {
+		fov = 40;
+	} else if(fov > 100) {
+		fov = 100;
 	}
 }
 
 function zoomOut() {
-	if( fov <= 100 ) {
+	if(fov <= 100) {
 		fov += 5;
-		camera.projectionMatrix = THREE.Matrix4.makePerspective( fov, window.innerWidth / window.innerHeight, 1, 1100 );
+		camera.projectionMatrix = THREE.Matrix4.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
 		render();
+	}
+	// keep field of view within bounds
+	if(fov < 40) {
+		fov = 40;
+	} else if(fov > 100) {
+		fov = 100;
 	}
 }
 
