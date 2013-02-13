cce930cca04d6e295bf8754ec48093c71ccae7b2
András Molnár <andras1024@gmail.com>
2013-02-13 03:53:08 +0100

    Restructured configuration loading.
    Signed-off-by: Matthew Petroff <matthew@mpetroff.net>

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -25,22 +25,28 @@
 document.addEventListener('contextmenu', onRightClick, false);
 
 // Declare variables
-var config, tour, popoutMode = false, hfov = 70, pitch = 0, yaw = 0, haov = 360,
-    vaov = 180, voffset = 0, renderer, isUserInteracting = false,
+var config, tourConfig = {}, configFromUrl, popoutMode = false, renderer, isUserInteracting = false,
     onMouseDownMouseX = 0, onMouseDownMouseY = 0, onMouseDownYaw = 0,
     onMouseDownPitch = 0, phi = 0, theta = 0, keysDown = new Array(10),
     fullWindowActive = false, loaded = false, error = false, isTimedOut = false, eventsadded = false,
-    about_box = document.getElementById('about_box'), autoRotate = false,
-    canvas = document.getElementById('canvas'), panoType = 'equirectangular',
+    about_box = document.getElementById('about_box'), 
+    canvas = document.getElementById('canvas'), 
     panoImage, panoSrc;
 
+var defaultConfig = {
+	hfov: 70, pitch: 0, yaw: 0, haov: 360,
+    vaov: 180, voffset: 0, autoRotate: false,
+	panoType: 'equirectangular', title: '',
+	author: ''
+};	
+	
 // Process options
 parseURLParameters();
 processOptions();
 
 // Initialize viewer
 function init() {
-    if(panoType == 'cubemap') {
+    if(config.panoType == 'cubemap') {
         panoImage = new Array();
         for(var i = 0; i < 6; i++) {
             panoImage.push(new Image());
@@ -51,7 +57,7 @@ function init() {
 
     function onImageLoad() {
         try {
-            renderer = new libpannellum.renderer(canvas, panoImage, panoType);
+            renderer = new libpannellum.renderer(canvas, panoImage, config.panoType);
         } catch (event) {
             // Show error message if WebGL is not supported
             anError();
@@ -89,7 +95,7 @@ function init() {
     }
     
     // Configure image loading
-    if(panoType == "cubemap") {
+    if(config.panoType == "cubemap") {
         // Quick loading counter for synchronous loading
         var itemsToLoad = 6;
         function loadCounter() {
@@ -136,15 +142,15 @@ function onDocumentMouseDown(event) {
     window.focus();
     
     // Turn off auto-rotation if enabled
-    autoRotate = false;
+    config.autoRotate = false;
     
     isUserInteracting = true;
     
     onPointerDownPointerX = event.clientX;
     onPointerDownPointerY = event.clientY;
     
-    onPointerDownYaw = yaw;
-    onPointerDownPitch = pitch; 
+    onPointerDownYaw = config.yaw;
+    onPointerDownPitch = config.pitch; 
     
     document.getElementById('page').className = 'grabbing';
 }
@@ -152,8 +158,8 @@ function onDocumentMouseDown(event) {
 function onDocumentMouseMove(event) {
     if (isUserInteracting) {
         //TODO: This should not only be FOV scaled but scaled to canvas size
-        yaw = (onPointerDownPointerX - event.clientX) * 0.0029 * hfov + onPointerDownYaw;
-        pitch = (event.clientY - onPointerDownPointerY) * 0.0029 * hfov + onPointerDownPitch;
+        config.yaw = (onPointerDownPointerX - event.clientX) * 0.0029 * config.hfov + onPointerDownYaw;
+        config.pitch = (event.clientY - onPointerDownPointerY) * 0.0029 * config.hfov + onPointerDownPitch;
         animate();
     }
 }
@@ -167,16 +173,16 @@ function onDocumentTouchStart(event) {
     onPointerDownPointerX = event.targetTouches[0].clientX;
     onPointerDownPointerY = event.targetTouches[0].clientY;
     
-    onPointerDownYaw = yaw;
-    onPointerDownPitch = pitch;
+    onPointerDownYaw = config.yaw;
+    onPointerDownPitch = config.pitch;
 }
 
 function onDocumentTouchMove(event) {
     // Override default action
     event.preventDefault();
         
-    yaw = (onPointerDownPointerX - event.targetTouches[0].clientX) * 0.1 + onPointerDownYaw;
-    pitch = (event.targetTouches[0].clientY - onPointerDownPointerY) * 0.1 + onPointerDownPitch;
+    config.yaw = (onPointerDownPointerX - event.targetTouches[0].clientX) * 0.1 + onPointerDownYaw;
+    config.pitch = (event.targetTouches[0].clientY - onPointerDownPointerY) * 0.1 + onPointerDownPitch;
     animate();
 }
 
@@ -189,13 +195,13 @@ function onDocumentMouseWheel(event) {
     
     if (event.wheelDeltaY) {
         // WebKit
-        setHfov(hfov -= event.wheelDeltaY * 0.05);
+        setHfov(config.hfov -= event.wheelDeltaY * 0.05);
     } else if (event.wheelDelta) {
         // Opera / Explorer 9
-        setHfov(hfov -= event.wheelDelta * 0.05);
+        setHfov(config.hfov -= event.wheelDelta * 0.05);
     } else if (event.detail) {
         // Firefox
-        setHfov(hfov += event.detail * 1.5);
+        setHfov(config.hfov += event.detail * 1.5);
     }
 }
 
@@ -204,7 +210,7 @@ function onDocumentKeyPress(event) {
     event.preventDefault();
     
     // Turn off auto-rotation if enabled
-    autoRotate = false;
+    config.autoRotate = false;
     
     // Record key pressed
     keynumber = event.keycode;
@@ -302,42 +308,42 @@ function keyRepeat() {
     // If up arrow or "w" is down
     if(keysDown[2] || keysDown[6]) {
         // Pan up
-        pitch += 1;
+        config.pitch += 1;
         animate();
     }
     
     // If down arrow or "s" is down
     if(keysDown[3] || keysDown[7]) {
         // Pan down
-        pitch -= 1;
+        config.pitch -= 1;
         animate();
     }
     
     // If left arrow or "a" is down
     if(keysDown[4] || keysDown[8]) {
         // Pan left
-        yaw -= 1;
+        config.yaw -= 1;
         animate();
     }
     
     // If right arrow or "d" is down
     if(keysDown[5] || keysDown[9]) {
         // Pan right
-        yaw += 1;
+        config.yaw += 1;
         animate();
     }
     
     // If clockwise auto-rotate
-    if(autoRotate == 'cw') {
+    if(config.autoRotate == 'cw') {
         // Pan left
-        yaw -= .25;
+        config.yaw -= .25;
         animate();
     }
     
     // If counter-clockwise auto-rotate
-    if(autoRotate == 'ccw') {
+    if(config.autoRotate == 'ccw') {
         // Pan right
-        yaw += .25;
+        config.yaw += .25;
         animate();
     }
 }
@@ -360,14 +366,14 @@ function animate() {
 
 function render() {
     if(loaded) {
-        if(yaw > 180) {
-            yaw -= 360;
-        } else if(yaw < -180) {
-            yaw += 360;
+        if(config.yaw > 180) {
+            config.yaw -= 360;
+        } else if(config.yaw < -180) {
+            config.yaw += 360;
         }
         
-        pitch = Math.max(-85, Math.min(85, pitch));
-        renderer.render(pitch * Math.PI / 180, yaw * Math.PI / 180, hfov * Math.PI / 180);
+        config.pitch = Math.max(-85, Math.min(85, config.pitch));
+        renderer.render(config.pitch * Math.PI / 180, config.yaw * Math.PI / 180, config.hfov * Math.PI / 180);
         
         renderHotSpots();
     }
@@ -377,7 +383,7 @@ function renderInit() {
     try {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        renderer.init(haov * Math.PI / 180, vaov * Math.PI / 180, voffset * Math.PI / 180);
+        renderer.init(config.haov * Math.PI / 180, config.vaov * Math.PI / 180, config.voffset * Math.PI / 180);
         
         animate();
         
@@ -387,6 +393,7 @@ function renderInit() {
             // Hide loading display
             document.getElementById('load_box').style.display = 'none';
             loaded = true;
+			render();
         }
     } catch(event) {
         // Panorama not loaded
@@ -453,21 +460,21 @@ function destroyHotSpots() {
 
 function renderHotSpots() {
     config.hotSpots.forEach(function(hs) {
-        var z = Math.sin(hs.pitch * Math.PI / 180) * Math.sin(pitch * Math.PI /
-            180) + Math.cos(hs.pitch * Math.PI / 180) * Math.cos((hs.yaw + yaw) *
-            Math.PI / 180) * Math.cos(pitch * Math.PI / 180);
+        var z = Math.sin(hs.pitch * Math.PI / 180) * Math.sin(config.pitch * Math.PI /
+            180) + Math.cos(hs.pitch * Math.PI / 180) * Math.cos((hs.yaw + config.yaw) *
+            Math.PI / 180) * Math.cos(config.pitch * Math.PI / 180);
         if((hs.yaw <= 90 && hs.yaw > -90 && z <= 0) ||
           (hs.yaw > 90 || hs.yaw <= -90 && z <= 0)) {
             hs.div.style.visibility = 'hidden';
         } else {
             hs.div.style.visibility = 'visible';
-            hs.div.style.top = -canvas.height / Math.tan(hfov * Math.PI / 360) *
-                (Math.sin(hs.pitch * Math.PI / 180) * Math.cos(pitch * Math.PI /
+            hs.div.style.top = -canvas.height / Math.tan(config.hfov * Math.PI / 360) *
+                (Math.sin(hs.pitch * Math.PI / 180) * Math.cos(config.pitch * Math.PI /
                 180) - Math.cos(hs.pitch * Math.PI / 180) * Math.cos((hs.yaw +
-                yaw) * Math.PI / 180) * Math.sin(pitch * Math.PI / 180)) / z /
+                config.yaw) * Math.PI / 180) * Math.sin(config.pitch * Math.PI / 180)) / z /
                 2 + canvas.height / 2 - 13.5 + 'px';
-            hs.div.style.left = -canvas.height / Math.tan(hfov * Math.PI / 360) *
-                Math.sin((hs.yaw + yaw) * Math.PI / 180) * Math.cos(hs.pitch *
+            hs.div.style.left = -canvas.height / Math.tan(config.hfov * Math.PI / 360) *
+                Math.sin((hs.yaw + config.yaw) * Math.PI / 180) * Math.cos(hs.pitch *
                 Math.PI / 180) / z / 2 + canvas.width / 2 - 13.5 + 'px';
         }
     });
@@ -495,62 +502,68 @@ function parseURLParameters() {
         }
     }
     json += '}';
-    config = JSON.parse(json);
+    configFromUrl = JSON.parse(json);
     
     // Check for JSON configuration file
-    for(var key in config) {
-        if(key == 'config') {
-            // Get JSON configuration file
-            var request = new XMLHttpRequest();
-            request.open('GET', config[key], false);
-            request.send();
-            var c = JSON.parse(request.responseText);
+    if(configFromUrl.config) {
+        // Get JSON configuration file
+        var request = new XMLHttpRequest();
+        request.open('GET', configFromUrl.config, false);
+        request.send();
+        var c = JSON.parse(request.responseText);
             
-            // Merge options
-            for(var k in c) {
-                if(!config[k]) {
-                    config[k] = c[k];
-                }
+        // Merge options
+        for(var k in c) {
+            if(!configFromUrl[k]) {
+                configFromUrl[k] = c[k];
             }
         }
     }
 	
+	var firstScene = null;
 	// Check for virtual tour configuration file
-    for(var key in config) {
-        if(key == 'tour') {
-            // Get JSON configuration file
-            var request = new XMLHttpRequest();
-            request.open('GET', config[key], false);
-            request.send();
-            tour = JSON.parse(request.responseText);
-			// Back up parameters readed from config file and URL for future use
-			tour.configFromUrl = config;
-			// Activating first scene if specified
-            if(tour.global.defaultScene){
-				loadSceneData(tour.global.defaultScene);
-			}else{
-				loadSceneData(null);
-			}
-        }
+    if(configFromUrl.tour) {
+        // Get JSON configuration file
+        var request = new XMLHttpRequest();
+        request.open('GET', configFromUrl.tour, false);
+        request.send();
+        tourConfig = JSON.parse(request.responseText);
+		// Activating first scene if specified
+        if(tourConfig.global.firstScene) {
+			firstScene = tourConfig.global.firstScene;
+		}
+		if(configFromUrl.firstScene) {
+			firstScene = configFromUrl.firstScene;
+		}
     }
+	
+	mergeConfig(firstScene);
 }
 
-function loadSceneData(sceneId){
-	// Merge data loaded from URL and config file + global data from tour file + scene specific data
-	config = tour.configFromUrl;
-	// Merge global options
-    for(var k in tour.global){
-        config[k] = tour.global[k];
+function mergeConfig(sceneId){
+	// Merge config: default config + global scene + selected scene + from url and cofig file
+	config = {};
+	// Merge default config
+    for(var k in defaultConfig){
+        config[k] = defaultConfig[k];
     }
-	// Merge scene options
-	if((sceneId != null) && (sceneId != '') && (tour.scenes) && (tour.scenes[sceneId]))
+	// Merge global config
+    for(var k in tourConfig.global){
+        config[k] = tourConfig.global[k];
+    }
+	// Merge current scene config
+	if((sceneId != null) && (sceneId != '') && (tourConfig.scenes) && (tourConfig.scenes[sceneId]))
 	{
-		for(var k in tour.scenes[sceneId]){
-			var scene = tour.scenes[sceneId];
+		var scene = tourConfig.scenes[sceneId];
+		for(var k in scene){			
 			config[k] = scene[k];
 		}
 		config.activeScene = sceneId;
 	}
+	// Merge url and config file
+    for(var k in configFromUrl){
+        config[k] = configFromUrl[k];
+    }
 }
 
 function processOptions() {
@@ -559,7 +572,9 @@ function processOptions() {
             case 'logo':
                 if(config[key] == 'yes') {
                     document.getElementById('pannellum_logo').style.display = 'inline';
-                }
+                } else {
+					document.getElementById('pannellum_logo').style.display = 'none';
+				}
                 break;
             
             case 'title':
@@ -591,29 +606,12 @@ function processOptions() {
                 break;
             
             case 'pitch':
-                pitch = config[key];
                 // Keep pitch within bounds
-                if(pitch < -85) {
-                    pitch = -85;
-                } else if(pitch > 85) {
-                    pitch = 85;
+                if(config.pitch < -85) {
+                    config.pitch = -85;
+                } else if(config.pitch > 85) {
+                    config.pitch = 85;
                 }
-                break;
-            
-            case 'yaw':
-                yaw = config[key];
-                break;
-            
-            case 'haov':
-                haov = config[key];
-                break;
-            
-            case 'vaov':
-                vaov = config[key];
-                break;
-            
-            case 'voffset':
-                voffset = config[key];
                 break;
             
             case 'autoload':
@@ -629,9 +627,10 @@ function processOptions() {
                 animate();
                 break;
             
+			// Accept both "autorotate" and "autoRotate" in config for compatibility
             case 'autorotate':
                 if(config[key] == 'cw' || config[key] == 'ccw') {
-                    autoRotate = config[key];
+                    config.autoRotate = config[key];
                 }
                 break;
             
@@ -651,8 +650,9 @@ function processOptions() {
                 license.width = '80px';
                 break;
             
+			// Accept both "type" and "panoType" in config for compatibility
             case 'type':
-                panoType = config[key];
+                config.panoType = config[key];
                 break;
             
             case 'panorama':
@@ -719,24 +719,24 @@ function fullScreenError() {
 
 function zoomIn(amount) {
     if(loaded) {
-        setHfov(hfov -= amount);
+        setHfov(config.hfov -= amount);
     }
 }
 
 function zoomOut(amount) {
     if(loaded) {
-        setHfov(hfov += amount);
+        setHfov(config.hfov += amount);
     }
 }
 
 function setHfov(i) {
     // Keep field of view within bounds
     if(i < 40) {
-        hfov = 40;
+        config.hfov = 40;
     } else if(i > 100) {
-        hfov = 100;
+        config.hfov = 100;
     } else {
-        hfov = i;
+        config.hfov = i;
     }
     
     animate();
@@ -754,7 +754,7 @@ function loadScene(sceneId){
 	// Destroy hotspots from previous scene
 	destroyHotSpots();
 	// Create the new config for the scene
-	loadSceneData(sceneId);
+	mergeConfig(sceneId);
 	// Reload scene
 	processOptions();
 	load();
