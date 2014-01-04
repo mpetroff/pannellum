b03300730da7b52277b8ce2b2f7e8fde9ece4f7a
Benjamin BERNARD <benvii@mdl29.net>
2014-01-04 15:37:51 +0100

    Correcting multi-resolution repeated tiles (artifacts on edges)

--- src/js/libpannellum.js
+++ src/js/libpannellum.js
@@ -490,6 +490,8 @@ function Renderer(canvas, image, imageType) {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.generateMipmap(gl.TEXTURE_2D);
         gl.bindTexture(gl.TEXTURE_2D, null);
     }
