36bc077102d78298c3fd85aacc0d1e96f01fa742
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2017-09-18 18:51:12 +0200

    create example string file and import it for testing

---
+++ src/js/strings.js
@@ -0,0 +1,12 @@
+var STRINGS = {
+    "ERROR_INVALID_IMG_TYPE": {
+        "de": "Error: Falscher Bild-Typ angegeben!",
+        "en": "Error: invalid image type specified!",
+        "ru": "ошибка: неправильный фото тип!"
+    }
+    //,
+    // "ERROR_NO_WEBGL": {
+    //     "de": "...",
+    //     "en": "..."
+    // }
+}
