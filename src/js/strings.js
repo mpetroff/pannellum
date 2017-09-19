d56a097ccf98e30cc72fb3e7e876574052695e52
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2017-09-20 01:10:07 +0200

    add error constant for when webGL is not supported
    include german, english and russian error descriptions

--- src/js/strings.js
+++ src/js/strings.js
@@ -3,10 +3,10 @@ var STRINGS = {
         "de": "Error: Falscher Bild-Typ angegeben!",
         "en": "Error: invalid image type specified!",
         "ru": "ошибка: неправильный фото тип!"
+    },
+    "ERROR_NO_WEBGL": {
+        "de": "Error: WebGL wird nicht unterstützt!",
+        "en": "Error: no WebGL support detected!",
+        "ru": "ошибка: нет функции WebGL!"
     }
-    //,
-    // "ERROR_NO_WEBGL": {
-    //     "de": "...",
-    //     "en": "..."
-    // }
 }
