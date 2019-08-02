mpetroff/pannellum

commit 12516e69b90e8ab4c6f42b64560aef92ae539bd0

Fix issue with mouse dragging causing jump around yaw limits (fixes #698)

src/js/pannellum.js
@@ -1399,12 +1399,6 @@ function render() {
     var tmpyaw;
 
     if (loaded) {
-        if (config.yaw > 180) {
-            config.yaw -= 360;
-        } else if (config.yaw < -180) {
-            config.yaw += 360;
-        }
-
         // Keep a tmp value of yaw for autoRotate comparison later
         tmpyaw = config.yaw;
 
@@ -1439,6 +1433,12 @@ function render() {
             config.yaw = Math.max(minYaw, Math.min(maxYaw, config.yaw));
         }
         
+        if (config.yaw > 180) {
+            config.yaw -= 360;
+        } else if (config.yaw < -180) {
+            config.yaw += 360;
+        }
+
         // Check if we autoRotate in a limited by min and max yaw
         // If so reverse direction
         if (config.autoRotate !== false && tmpyaw != config.yaw &&

commit d773cc9f60974bae65a38da5ead56abeb62ea963

Fix regression with auto-rotate stopping early (fixes #764).
This regression was introduced in 12516e69b90e8ab4c6f42b64560aef92ae539bd0.

src/js/pannellum.js
@@ -1409,6 +1409,15 @@ function render() {
     if (loaded) {
         var canvas = renderer.getCanvas();
 
+        if (config.autoRotate !== false) {
+            // When auto-rotating this check needs to happen first (see issue #764)
+            if (config.yaw > 180) {
+                config.yaw -= 360;
+            } else if (config.yaw < -180) {
+                config.yaw += 360;
+            }
+        }
+
         // Keep a tmp value of yaw for autoRotate comparison later
         tmpyaw = config.yaw;
 
@@ -1442,10 +1451,14 @@ function render() {
             config.yaw = Math.max(minYaw, Math.min(maxYaw, config.yaw));
         }
         
-        if (config.yaw > 180) {
-            config.yaw -= 360;
-        } else if (config.yaw < -180) {
-            config.yaw += 360;
+        if (!(config.autoRotate !== false)) {
+            // When not auto-rotating, this check needs to happen after the
+            // previous check (see issue #698)
+            if (config.yaw > 180) {
+                config.yaw -= 360;
+            } else if (config.yaw < -180) {
+                config.yaw += 360;
+            }
         }
 
         // Check if we autoRotate in a limited by min and max yaw