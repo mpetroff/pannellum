46c89c4155964a06303eea52ebce78879f20f18a
Matthew Petroff <matthew@mpetroff.net>
2019-08-07 00:05:14 -0400

    Revert "Another attempt at headless testing with Travis CI."
    This reverts commits 2d0230dd4e1ae77a1d7dd8705fedf0f5f4c40156,
    9e23ee0b5708bb96552d15571ec966f604a43301,
    e996faa86c9dcc4c3e4a859cbb75567d4e3ace91,
    and d204ac06d5127135258584d886b089f7092c5856.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -33,11 +33,24 @@ thread.start()
 
 # Create a new instance of the Firefox driver
 print("Starting web driver...")
-fp = webdriver.FirefoxProfile()
-fp.set_preference("layout.css.devPixelsPerPx", "1.0")
-fp.set_preference("devtools.console.stdout.content", True)
-driver = webdriver.Firefox(firefox_profile=fp)
-driver.set_window_size(800, 600)
+if os.environ.get("TRAVIS_JOB_NUMBER"):
+    # Configuration for Travis CI / Sauce Labs testing
+    driver = webdriver.Remote(
+        command_executor="https://ondemand.saucelabs.com:443/wd/hub",
+        desired_capabilities={
+            "username": os.environ["SAUCE_USERNAME"],
+            "accessKey": os.environ["SAUCE_ACCESS_KEY"],
+            "tunnel-identifier": os.environ["TRAVIS_JOB_NUMBER"],
+            "build": os.environ["TRAVIS_JOB_NUMBER"],
+            "browserName": "firefox",
+            "seleniumVersion": "3.141.0",
+        },
+    )
+else:
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("layout.css.devPixelsPerPx", "1.0")
+    driver = webdriver.Firefox(firefox_profile=fp)
+    driver.set_window_size(800, 600)
 
 
 def run_tests():
