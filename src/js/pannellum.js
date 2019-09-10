6d12ef7968a484eb95802411c277260c3b867c5b
Matthew Petroff <matthew@mpetroff.net>
2019-09-10 10:57:30 -0400

    Temporarily revert "Move `load` event to fire after rendering instead of before (#768)."
    This reverts commit df4c45dd332921b96fb3fa021cdab4dd06f75d96 temporarily to
    push a new point release.

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -1685,10 +1685,10 @@ function renderInitCallback() {
         preview = undefined;
     }
     loaded = true;
-    
-    animateInit();
 
     fireEvent('load');
+    
+    animateInit();
 }
 
 /**
