d86d278ebde2172d46d64a075621e766a27be7f1
Igor Barzhak <ibarzhak@gmail.com>
2020-10-21 19:37:36 +0300

    Fix unsafe link
    https://web.dev/external-anchors-use-rel-noopener/?utm_source=lighthouse&utm_medium=devtools

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -160,7 +160,7 @@ uiContainer.appendChild(dragFix);
 // Display about information on right click
 var aboutMsg = document.createElement('span');
 aboutMsg.className = 'pnlm-about-msg';
-aboutMsg.innerHTML = '<a href="https://pannellum.org/" target="_blank">Pannellum</a>';
+aboutMsg.innerHTML = '<a href="https://pannellum.org/" target="_blank" rel="noopener noreferrer nofollow">Pannellum</a>';
 uiContainer.appendChild(aboutMsg);
 dragFix.addEventListener('contextmenu', aboutMessage);
 
