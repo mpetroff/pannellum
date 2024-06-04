mpetroff/pannellum

commit 14208198aa94c25a53aeb603132d02769128c089

Add support for initial view settings (#918)
* Add support for initial view settings

* Independent pitch/yap/hfov configure from xmp

src/js/pannellum.js
@@ -577,7 +577,10 @@ function parseGPanoXMP(image, url) {
                 topPixels: getTag('GPano:CroppedAreaTopPixels'),
                 heading: getTag('GPano:PoseHeadingDegrees'),
                 horizonPitch: getTag('GPano:PosePitchDegrees'),
-                horizonRoll: getTag('GPano:PoseRollDegrees')
+                horizonRoll: getTag('GPano:PoseRollDegrees'),
+                pitch: getTag('GPano:InitialViewPitchDegrees'),
+                yaw: getTag('GPano:InitialViewHeadingDegrees'),
+                hfov: getTag('GPano:InitialHorizontalFOVDegrees')
             };
             
             if (xmp.fullWidth !== null && xmp.croppedWidth !== null &&
@@ -605,7 +608,12 @@ function parseGPanoXMP(image, url) {
                         config.horizonRoll = xmp.horizonRoll;
                 }
                 
-                // TODO: add support for initial view settings
+                if (xmp.pitch != null && specifiedPhotoSphereExcludes.indexOf('pitch') < 0)
+                    config.pitch = xmp.pitch;
+                if (xmp.yaw != null && specifiedPhotoSphereExcludes.indexOf('yaw') < 0)
+                    config.yaw = xmp.yaw;
+                if (xmp.hfov != null && specifiedPhotoSphereExcludes.indexOf('hfov') < 0)
+                    config.hfov = xmp.hfov;
             }
         }
         

commit 011497a7bddf1dfd0e15f819bedf6aa0b27ef591

Don't override pitch / yaw / hfov from GPano XML.
Regression in 14208198aa94c25a53aeb603132d02769128c089.

src/js/pannellum.js
@@ -629,11 +629,11 @@ function parseGPanoXMP(image, url) {
                         config.horizonRoll = xmp.horizonRoll;
                 }
                 
-                if (xmp.pitch != null && specifiedPhotoSphereExcludes.indexOf('pitch') < 0)
+                if (xmp.pitch !== null && specifiedPhotoSphereExcludes.indexOf('pitch') < 0)
                     config.pitch = xmp.pitch;
-                if (xmp.yaw != null && specifiedPhotoSphereExcludes.indexOf('yaw') < 0)
+                if (xmp.yaw !== null && specifiedPhotoSphereExcludes.indexOf('yaw') < 0)
                     config.yaw = xmp.yaw;
-                if (xmp.hfov != null && specifiedPhotoSphereExcludes.indexOf('hfov') < 0)
+                if (xmp.hfov !== null && specifiedPhotoSphereExcludes.indexOf('hfov') < 0)
                     config.hfov = xmp.hfov;
             }
         }
@@ -2111,7 +2111,7 @@ function renderHotSpots() {
 function mergeConfig(sceneId) {
     config = {};
     var k, s;
-    var photoSphereExcludes = ['haov', 'vaov', 'vOffset', 'northOffset', 'horizonPitch', 'horizonRoll'];
+    var photoSphereExcludes = ['haov', 'vaov', 'vOffset', 'northOffset', 'horizonPitch', 'horizonRoll', 'pitch', 'yaw', 'hfov'];
     specifiedPhotoSphereExcludes = [];
     
     // Merge default config