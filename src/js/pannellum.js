2f9eac0d7dff85f20d2c44846a84b1e1935c7ffe
Robert Augusteijn <git@augusteijn.com>
2025-06-06 12:45:29 +0200

    Update orientationYawOffset on scene change (fixes #419)
    When using device orientation for pointing control and loading a new scene, the new config.yaw was not taken into account. This may cause undesired jumps in viewing direction.
    
    This change triggers recalculation of the orientationYawOffset with the config.yaw of the newly loaded scene.

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -2554,6 +2554,10 @@ function loadScene(sceneId, targetPitch, targetYaw, targetHfov, fadeDone) {
     if (workingHfov !== undefined) {
         config.hfov = workingHfov;
     }
+    
+    // Trigger recalculation of orientationYawOffset in orientationListener()
+    if (orientation === true) orientation = 10;
+	
     fireEvent('scenechange', sceneId);
     load();
 }
